16886b0058bb46fa29f5b8c71e69d3e844bdb41d
Marc Zottner <marc@zottner.com>
2015-01-24 11:39:47 +0100

    Update virtualbox_spec.rb

--- spec/classes/virtualbox_spec.rb
+++ spec/classes/virtualbox_spec.rb
@@ -9,11 +9,13 @@ describe 'virtualbox' do
     })
   end
   it do
-    should contain_package('VirtualBox-4.3.14-95030').with({
-      :ensure   => 'installed',
-      :source   => 'http://download.virtualbox.org/virtualbox/4.3.14/VirtualBox-4.3.14-95030-OSX.dmg',
-      :provider => 'pkgdmg',
-      :require  => 'Exec[Kill Virtual Box Processes]',
+    version = "4.3.20"
+    patch_level = "96966"
+    should contain_package("VirtualBox-${version}-${patch_level}").with({
+      :ensure   => "installed",
+      :source   => "http://download.virtualbox.org/virtualbox/${version}/VirtualBox-${version}-${patch_level}-OSX.dmg",
+      :provider => "pkgdmg",
+      :require  => "Exec[Kill Virtual Box Processes]",
     })
   end
 end
